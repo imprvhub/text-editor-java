504e64f4a70dbd5514cdfefadb95f21e2543d231
Iván <ivanflowtramites@gmail.com>
2023-09-09 19:30:33 -0300

    Initial commit

---
+++ Text Editor (Java)/task/src/editor/ApplicationRunner.java
@@ -0,0 +1,12 @@
+// Text Editor (Java / Java Desktop Application Developer) -
+// Graduate Project Completed By Iván Luna, September 9, 2023. -
+// For Hyperskill (Jet Brains Academy). Course: Java Desktop Application Developer.
+
+package editor;
+
+
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        new TextEditor();
+    }
+}
